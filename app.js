c77703d589fd6de4c819ffe53d0d0f45f744628e
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-07-05 06:33:40 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Using IIFE for Implementing Module Pattern to keep the Local Space for the JS Variables
+(function() {
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var serverUrl = "/",
+        comments = [],
+        pusher = new Pusher('Sy2wvV9xgxnjKuGAQ5bTWWZF5NwJj0b2lC5QYLZZXkQ', {
+          cluster: 'ap2',
+          encrypted: true
+        }),
+        // Subscribing to the 'flash-comments' Channel
+        channel = pusher.subscribe('flash-comments');
+
+})();
